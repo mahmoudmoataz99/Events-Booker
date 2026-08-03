49ac343570cc33c20bcdf595aef5f2622278ac09
Mahmoud Moataz <mahmoudmoataz99@gmail.com>
2026-08-03 14:47:04 +0300

    Update SwiperElement.jsx

--- src/Components/SwiperElement.jsx
+++ src/Components/SwiperElement.jsx
@@ -16,7 +16,7 @@ function SwiperElement(props) {
       {/* Text Content */}
       <div className="absolute bottom-20 right-20 text-white z-10 space-y-4 text-start">
         <h1 className="text-xl md:text-4xl lg:text-7xl font-bold">{props.title}</h1>
-        <h2 className="text-lg md:text-2xl">{new Date(props.date).toUTCString().replace("00:00:00 GMT", "")} | {props.time}</h2>
+        <h2 className="text-lg md:text-2xl">{new Date(props.date).toUTCString().replace("00:00:00 GMT", "")}</h2>
         <Link to={`/event/${props.id}`}
           className="flex justify-center items-center gap-x-4 text-xl md:text-2xl bg-purple-600 p-3 rounded-lg hover:bg-purple-800">
           <FaTicketAlt /> Book Now
